ea20d00903b9e43bb6a82d9d0aa5f806b335fe95
RakshitRahar <rakshitrahar143@gmail.com>
2025-06-17 10:30:05 +0530

    with basic react

---
+++ p1/script.js
@@ -0,0 +1,34 @@
+const loginForm = () => {
+    const str = `<div>
+    <h3>Login Form</h3>
+    <form action="">
+    <input type="text" placeholder="Enter yout email"></input><br><br>
+    <input type="text" placeholder="Enter yout password"></input><br>
+    </form>
+    <p><button onclick='showHome()'>Submit</button></p>
+    <p><button onclick='registerForm()'>Create Account</button></p>
+    `
+    root.innerHTML = str + "</div>"
+}
+
+const registerForm = () => {
+     const str = `<div>
+    <h3>Registration Form</h3>
+    <form action="">
+    <input type="text" placeholder="Enter yout name" ></input><br><br>
+    <input type="text" placeholder="Enter yout email"></input><br><br>
+    <input type="text" placeholder="Enter yout password"></input><br>
+    </form>
+    <p><button onclick='loginForm()'>Submit</button></p>
+    <p><button onclick='loginForm()'>Already a member? Login here...</button></p>
+    `
+    root.innerHTML = str + "</div>"
+}
+
+const showHome = () => {
+     const str = `<div>
+    <h3>Welcome</h3>
+    <p><button onclick='loginForm()'>Logout</button></p>
+    `
+    root.innerHTML = str + "</div>"
+}
